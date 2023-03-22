4a417801f3c78a575da11f849d9eedc2a65f07d9
JayRow23 <Jayokaitei@gmail.com>
2023-03-21 22:35:32 -0400

    alpha charaters checker

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - function is to check for alphabet charater
+ *
+ * @c: parameter to be checked
+ *
+ * Return: 1 if it is an alphabet otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
+
