27de071fe9ddf13f51a9864d7f4b4ed52e6348a0
JayRow23 <Jayokaitei@gmail.com>
2023-03-21 20:39:31 -0400

    alphabet printing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print alphabet in lower
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
