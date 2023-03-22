84dcc94f79b8129cc7dd21d53fcc887de2002f60
JayRow23 <Jayokaitei@gmail.com>
2023-03-22 17:01:21 -0400

    the absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
  * _abs - function that compute the absolate value of an integer
+ *@n: character checker
  *
- * Return: Always 0
+ * Return: abs of num
  */
-int _abs(int
+int _abs(int n)
 {
-	
-})
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
