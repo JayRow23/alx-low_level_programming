3ba4f502444747cdc4d1d28cbd566fd3ba59b882
JayRow23 <Jayokaitei@gmail.com>
2023-03-25 14:49:03 -0400

    positive and negative checker

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,12 +2,17 @@
 /**
  * positive_or_negative - checking the correct output when given a case of 0
  * @i: parameter check for positive or negative
+ * if the number is greater than 0: is positive, if the number is 0: is zero
+ * if the number is less than 0: is negative, followed by a new line
  * Return: Always 0
  */
+
 void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
-	else
+	else if (i < 0)
 		printf("%d is positive\n", i);
+	else
+		printf("%d is zero\n", i);
 }
