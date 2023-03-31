0c8c7845e56b670f252e3fe29573ff682a305564
JayRow23 <Jayokaitei@gmail.com>
2023-03-31 07:13:08 -0400

    comparetion of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - comparetion of two strings
+ * @s1: strings
+ * @s2: strings
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
+	{
+		if (s1[j] != s2[j])
+		{
+			if (s1[j] < s2[j])
+				return (s1[j] - s2[j]);
+			else if (s1[j] > s2[j])
+				return (s1[j] - s2[j]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
