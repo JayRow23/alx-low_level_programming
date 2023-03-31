34f8a266ee7fbc17ba581e7f829e9a2c599676e2
JayRow23 <Jayokaitei@gmail.com>
2023-03-31 18:21:16 -0400

    compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int j;
 
-	for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
+	j = 0;
+	while (s1[j] != '\0' && s2[j] != '\0')
 	{
 		if (s1[j] != s2[j])
 		{
-			if (s1[j] < s2[j])
-				return (s1[j] - s2[j]);
-			else if (s1[j] > s2[j])
-				return (s1[j] - s2[j]);
+			return (s1[j] - s2[j]);
 		}
-		else
-			return (0);
+		j++;
 	}
 	return (0);
 }
