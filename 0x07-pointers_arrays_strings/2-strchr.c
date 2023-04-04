e78b879d13a6a61f8bd033f394bed3293560e1a9
JayRow23 <Jayokaitei@gmail.com>
2023-04-04 07:51:59 -0400

    gets the length of a prefix substring.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: location of strings
+ * @c: location of char
+ * Return: a pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int jet;
+
+	for (jet = 0; s[jet] >= '\0'; jet++)
+	{
+		if (s[jet] == c)
+			return (s + jet);
+	}
+	return ('\0');
+}
