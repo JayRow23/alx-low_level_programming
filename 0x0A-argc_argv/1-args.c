2c65f42a7a88a9232b72f5289472ed932ba38b87
JayRow23 <Jayokaitei@gmail.com>
2023-04-09 14:30:49 -0400

    program with arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main- program that prints a number followed by newline
+ * @argv: arguments
+ * @argc: number of argument
+ * Reture: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
