0919fad43f663f1636bba9995ce03b968bdcc583
JayRow23 <Jayokaitei@gmail.com>
2023-04-09 13:59:20 -0400

    what you answer to-task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- program that prints its name, followed by a new line
+ * @argc: number of argument
+ * @argv: argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0x0A. C - argc, argv - New Project - New Chapter
