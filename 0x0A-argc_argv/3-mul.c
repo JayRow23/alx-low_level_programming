3d7c076502035f63236e82417e84cc1b815cd95b
JayRow23 <Jayokaitei@gmail.com>
2023-04-10 10:49:00 -0400

    Jmulti

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * main- print the result of the multiplication, followed by a new line
  * @argv: arguments
  * @argc: number of arguments
- * @jrow: argv *argv param
  * Return: Always 0 if true, 1 if false.
  */
-int main(int argc, char *argv[], jrow)
+int main(int argc, char *argv[])
 {
 	int j, k;
 
 	if (argc == 1)
 	{
-		j = jrow(argv[1]);
-		k = jrow(argv[2]);
+		j = atoi(argv[1]);
+		k = atoi(argv[2]);
 		printf("%d\n", j * k);
 		return (0);
 	}
